437d56563df45575b3c356b440fdc7094d930d77
Juan <juan@dataspartan.com>
2023-04-28 10:03:14 +0200

    chore: basic layout

--- src/app/ui/layout/layout.module.ts
+++ src/app/ui/layout/layout.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { RouterModule } from '@angular/router';
+import { SideNavMenuComponent } from './side-nav-menu/side-nav-menu.component';
+import { LayoutHeaderComponent } from './layout-header/layout-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    SideNavMenuComponent,
+    LayoutHeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    LayoutComponent
   ]
 })
 export class LayoutModule { }
