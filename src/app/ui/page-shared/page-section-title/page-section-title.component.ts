70499f0ad3eaf02e9ce12ad8f209d66ffb4837a8
Juan <juan@dataspartan.com>
2023-04-28 10:24:56 +0200

    chore: page-shared module

---
+++ src/app/ui/page-shared/page-section-title/page-section-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-section-title',
+  templateUrl: './page-section-title.component.html',
+  styleUrls: ['./page-section-title.component.scss']
+})
+export class PageSectionTitleComponent {
+
+}
